e38454546b018ef1c83e63402788fbccb026d02a
fernanda de leon <fernanda.-deleon@hotmail.com>
2024-01-01 22:56:16 +0100

    Intercept and resend attack

--- main.py
+++ main.py
@@ -207,3 +207,126 @@ if final_alice_key[0:num_bits_to_compare] == final_bob_key[0:num_bits_to_compare
 
 else:
   print('\n\nEve was listening, we need to use a different channel!')
+  
+# Intercept and Resend Attack
+  
+encode_gates = {0: cirq.I, 1: cirq.X}
+basis_gates = {'Z': cirq.I, 'X': cirq.H}
+
+num_bits = 100
+qubits = cirq.NamedQubit.range(num_bits, prefix = 'q')
+
+alice_key = choices([0, 1], k = num_bits)
+
+print('Alice\'s initial key: ', alice_key)
+
+alice_bases = choices(['Z', 'X'], k = num_bits)
+
+print('\nAlice\'s randomly chosen bases: ', alice_bases)
+
+alice_circuit = cirq.Circuit()
+
+for bit in range(num_bits):
+
+  encode_value = alice_key[bit]
+  encode_gate = encode_gates[encode_value]
+
+  basis_value = alice_bases[bit]
+  basis_gate = basis_gates[basis_value]
+
+  qubit = qubits[bit]
+  alice_circuit.append(encode_gate(qubit))
+  alice_circuit.append(basis_gate(qubit))
+
+eve_bases = choices(['Z', 'X'], k = num_bits)
+print('Eve\'s randomly chosen bases: ', eve_bases)
+
+eve_circuit = cirq.Circuit()
+
+for bit in range(num_bits):
+
+  basis_value = eve_bases[bit]
+  basis_gate = basis_gates[basis_value]
+
+  qubit = qubits[bit]
+  eve_circuit.append(basis_gate(qubit))
+
+eve_circuit.append(cirq.measure(qubits, key = 'eve key'))
+
+eve_intercept_circuit = alice_circuit + eve_circuit
+
+sim = cirq.Simulator()
+results = sim.run(eve_intercept_circuit)
+eve_key = results.measurements['eve key'][0]
+
+print('\nEve\'s initial key: ', eve_key)
+
+alice_circuit = cirq.Circuit()
+
+for bit in range(num_bits):
+
+  encode_value = eve_key[bit]
+  encode_gate = encode_gates[encode_value]
+
+  basis_value = eve_bases[bit]
+  basis_gate = basis_gates[basis_value]
+
+  qubit = qubits[bit]
+  alice_circuit.append(encode_gate(qubit))
+  alice_circuit.append(basis_gate(qubit))
+
+print('\nAlice\'s Phase 1 circuit after Eve\'s interception:\n', alice_circuit)
+
+bob_bases = choices(['Z', 'X'], k = num_bits)
+print('Bob\'s randomly chosen bases: ', bob_bases)
+
+bob_circuit = cirq.Circuit()
+
+for bit in range(num_bits):
+
+  basis_value = bob_bases[bit]
+  basis_gate = basis_gates[basis_value]
+
+  qubit = qubits[bit]
+  bob_circuit.append(basis_gate(qubit))
+  
+bob_circuit.append(cirq.measure(qubits, key = 'bob key'))
+
+print(bob_circuit)
+
+bb84_circuit = alice_circuit + bob_circuit
+
+sim = cirq.Simulator()
+results = sim.run(bb84_circuit)
+bob_key = results.measurements['bob key'][0]
+
+print('\nBob\'s initial key: ', bob_key)
+
+final_alice_key = []
+final_bob_key = []
+final_eve_key = []
+
+for bit in range(num_bits):
+
+  if alice_bases[bit] == bob_bases[bit]:
+    final_alice_key.append(alice_key[bit])
+    final_bob_key.append(bob_key[bit])
+    final_eve_key.append(eve_key[bit])
+
+print('\nAlice\'s key: ', final_alice_key)
+print('Bob\'s key: ', final_bob_key)
+print('Eve\'s key: ', final_eve_key)
+
+num_bits_to_compare = int(len(final_alice_key) * .5)
+if final_alice_key[0:num_bits_to_compare] == final_bob_key[0:num_bits_to_compare]:
+  final_alice_key = final_alice_key[num_bits_to_compare:]
+  final_bob_key = final_bob_key[num_bits_to_compare:]
+  final_eve_key = final_eve_key[num_bits_to_compare:]
+
+  print('\n\nWe can use our keys!')
+  print('Alice Key: ', final_alice_key)
+  print('Bob Key: ', final_bob_key)
+  print('Eve Key: ', final_eve_key)
+
+else:
+  print('\n\nEve was listening, we need to use a different channel!')
