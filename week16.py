10e861419517c6c6507f9f53bb82059137d32e14
fernanda de leon <fernanda.-deleon@hotmail.com>
2024-02-14 23:45:46 +0100

    Grover iteration

--- week16.py
+++ week16.py
@@ -26,3 +26,49 @@ def make_oracle(qubits, ancilla, marked_bitstring):
     for (q, bit) in zip(qubits, marked_bitstring):
         if not bit:
             yield (cirq.X(q))
+            
+    def grover_iteration(qubits, ancilla, marked_bitstring, reps=1):
+    """Performs one round of the Grover iteration."""
+    circuit = cirq.Circuit()
+
+    # Create an equal superposition over input qubits.
+    circuit.append(cirq.H.on_each(*qubits))
+
+    # Put the output qubit in the |-⟩ state.
+    circuit.append([cirq.X(ancilla), cirq.H(ancilla)])
+    for r in range(reps):
+        # Query the oracle.
+        # circuit.append(oracle)
+        for (q, bit) in zip(qubits, marked_bitstring):
+            if not bit:
+                circuit.append([cirq.X(q)])
+        # Do the Toffoli. change this to MCX for a generalized oracle
+        #yield (cirq.TOFFOLI(qubits[0], qubits[1], ancilla))
+        # controls = len(qubits) -1
+        controls = len(qubits)
+        mcx_gate = cirq.ControlledGate(sub_gate=cirq.X, num_controls=controls)
+        circuit.append([mcx_gate(*qubits, ancilla)])
+
+        # Negate zero bits, if necessary.
+        for (q, bit) in zip(qubits, marked_bitstring):
+            if not bit:
+                circuit.append([cirq.X(q)])
+
+        # Construct Grover operator.
+        circuit.append(cirq.H.on_each(*qubits))
+        circuit.append(cirq.X.on_each(*qubits))
+
+        # circuit.append(cirq.H.on(qubits[-1]))
+        controls = len(qubits)
+        mcx_gate = cirq.ControlledGate(sub_gate=cirq.X, num_controls=controls)
+        mcx_op = mcx_gate(*qubits, ancilla)
+        circuit.append(mcx_op)
+        # circuit.append(cirq.H.on(qubits[-1]))
+
+        circuit.append(cirq.X.on_each(*qubits))
+        circuit.append(cirq.H.on_each(*qubits))
+
+    # Measure the input register.
+    circuit.append(cirq.measure(*qubits, key="result"))
+
+    return circuit
