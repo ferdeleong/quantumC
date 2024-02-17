2b18b7a0b4c6fa356283679de752bbffa911a98c
fernanda de leon <fernanda.-deleon@hotmail.com>
2024-02-18 00:22:59 +0100

    Simulator using noise model

--- week17.py
+++ week17.py
@@ -5,3 +5,12 @@ circuit = cirq.Circuit()
 circuit.append(cirq.H(qubits[0]))
 circuit.append(cirq.CNOT(qubits[0], qubits[1]))
 circuit.append(cirq.measure(qubits))
+
+# Create a simulator that uses the noise model
+simulator = cirq.Simulator()
+result = simulator.run(circuit, repetitions=1000)
+
+# Get the results
+hist = cirq.plot_state_histogram(result, plt.subplot(), title = 'Qubit States', xlabel = 'States', ylabel = 'Occurrences', tick_label=binary_labels(2))
+
+plt.show()
